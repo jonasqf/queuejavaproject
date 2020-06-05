a0b7a72cb16ddf48bf0f98d46917a5f9efd5ff66
jonasqf <ntconsult.jonas@agibank.com.br>
2020-06-05 16:35:23 -0300

    first commit of this project

---
+++ src/main/java/com/queueproject/queuejavaproject/QueuejavaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.queueproject.queuejavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuejavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuejavaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queueproject/queuejavaproject/QueuejavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queueproject.queuejavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuejavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
